2530c3a37fe186c4511077e26c7304d4d8642a85
Leon-h-a <lekileo@gmail.com>
2024-10-27 19:47:14 +0000

    feat(routes/ui): add logic for checking if dirs exist
    UI contains input fields for Downloads and Movies directory.
    Upon user entering its dir defs, FE requests data validity check
    on '/plugins/jellyfork/run' which responds with Ok or BadRequest
    in case requested directories do not exist on local machine.

--- JellyFork/Plugin.cs
+++ JellyFork/Plugin.cs
@@ -1,25 +1,36 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using Jellyfin.Plugin.Template.Configuration;
+using Jellyfin.Plugin.JellyFork.Configuration;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
+using Microsoft.Extensions.Logging;
 
-namespace Jellyfin.Plugin.Template;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+
+namespace Jellyfin.Plugin.JellyFork;
 
 public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
 {
-    public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+    private readonly ILogger<Plugin> _logger;
+
+    public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer, ILogger<Plugin> logger)
         : base(applicationPaths, xmlSerializer)
     {
         Instance = this;
+        _logger = logger;
     }
     public override string Name => "JellyFork";
     public override string Description => "Move and rename new downloads to media directory";
     public override Guid Id => Guid.Parse("6aabd9ed-969f-4225-b316-6b3b33a2fddb");
-
     public static Plugin? Instance { get; private set; }
 
     public IEnumerable<PluginPageInfo> GetPages()
@@ -34,3 +45,4 @@ public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
         ];
     }
 }
+
