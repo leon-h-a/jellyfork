ffc8fa7d7870eac133172b00c38f6f977e3de79c
Leon-h-a <lekileo@gmail.com>
2024-11-18 02:02:16 +0000

    feat: add file readouts based on downloads dir conf

--- JellyFork/JellyForkController.cs
+++ JellyFork/JellyForkController.cs
@@ -1,11 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-
 using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Collections;
-using MediaBrowser.Common.Plugins;
-
+using System.Text.Json;
 
 namespace Jellyfin.JellyForkPlugin.JellyFork
 {
@@ -15,39 +13,83 @@ namespace Jellyfin.JellyForkPlugin.JellyFork
     {
         private readonly ILogger<JellyForkController> _logger;
 
-        public JellyForkController(ILogger<JellyForkController> logger) 
+        public JellyForkController(ILogger<JellyForkController> logger)
         {
             _logger = logger;
         }
 
-        [HttpPost("getMedia")]
+        [HttpPost("getmedia")]
         public IActionResult Run([FromForm] string inputDirectory)
         {
-            _logger.LogInformation(inputDirectory);
-            if (!Directory.Exists(inputDirectory)) {
+            _logger.LogInformation($"input: {inputDirectory}");
+
+            if (Directory.Exists(inputDirectory))
+            {
+                var directoryTree = GetDirectoryTree(inputDirectory);
+                string json = JsonSerializer.Serialize(directoryTree, new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                });
+
+                _logger.LogInformation(json);
+                return Ok(new { alert = "Configuration saved successfully!", data = json });
+            }
+            else
+            {
                 _logger.LogInformation("Downloads dir fail");
                 return BadRequest(new { alert = "Downloads directory does not exist" });
-            } else {
-                _logger.LogInformation("OK");
-                return Ok(new { alert = "Configuration saved successfully!" });
             }
-
         }
 
-        /* [HttpGet("getDefault")] */
-        /* public IActionResult GetConfiguration() */
-        /* { */
-        /*     if (_plugin != null) */
-        /*     { */
-        /*         var config = _plugin.Configuration; */
-        /*         return Ok(new */
-        /*         { */
-        /*             InputDirectory = config.InputDirectory, */
-        /*             OutputDirectory = config.OutputDirectory */
-        /*         }); */
-        /*     } */
+        private static DirectoryNode GetDirectoryTree(string path)
+        {
+            var directoryInfo = new DirectoryInfo(path);
 
-        /*     return NotFound(new { alert = "Plugin configuration not found" }); */
-        /* } */
+            if (directoryInfo.Attributes.HasFlag(FileAttributes.Hidden))
+            {
+                return null;
+            }
+
+            var directoryNode = new DirectoryNode
+            {
+                Name = directoryInfo.Name,
+                FullPath = directoryInfo.FullName,
+                Files = new List<FileNode>(),
+                Subdirectories = new List<DirectoryNode>()
+            };
+
+            foreach (var file in directoryInfo.GetFiles())
+            {
+                if (!file.Attributes.HasFlag(FileAttributes.Hidden))
+                {
+                    directoryNode.Files.Add(new FileNode
+                    {
+                        Name = file.Name,
+                        FullPath = file.FullName
+                    });
+                }
+            }
+
+            foreach (var subdirectory in directoryInfo.GetDirectories())
+            {
+                directoryNode.Subdirectories.Add(GetDirectoryTree(subdirectory.FullName));
+            }
+
+            return directoryNode;
+        }
+
+        private class DirectoryNode
+        {
+            public string Name { get; set; }
+            public string FullPath { get; set; }
+            public List<FileNode> Files { get; set; }
+            public List<DirectoryNode> Subdirectories { get; set; }
+        }
+
+        private class FileNode
+        {
+            public string Name { get; set; }
+            public string FullPath { get; set; }
+        }
     }
 }
