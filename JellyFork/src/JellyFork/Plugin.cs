33de542c1eae572e1866701670c39c5f1b3f4d04
Leon-ha <lekileo@gmail.com>
2024-12-02 13:04:48 +0100

    feat: initial

---
+++ JellyFork/src/JellyFork/Plugin.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Jellyfin.Plugin.Template.Configuration;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Serialization;
+
+namespace Jellyfin.Plugin.Template;
+
+public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+{
+    public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+        : base(applicationPaths, xmlSerializer)
+    {
+        Instance = this;
+    }
+    public override string Name => "JellyFork";
+    public override string Description => "Move and rename new downloads to media directory";
+    public override Guid Id => Guid.Parse("6aabd9ed-969f-4225-b316-6b3b33a2fddb");
+
+    public static Plugin? Instance { get; private set; }
+
+    public IEnumerable<PluginPageInfo> GetPages()
+    {
+        return
+        [
+            new PluginPageInfo
+            {
+                Name = Name,
+                EmbeddedResourcePath = string.Format(CultureInfo.InvariantCulture, "{0}.Configuration.configPage.html", GetType().Namespace)
+            }
+        ];
+    }
+}
