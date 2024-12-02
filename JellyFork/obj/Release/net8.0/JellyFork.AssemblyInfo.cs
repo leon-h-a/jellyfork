ca10d9b4d823288a5e60438529ae2564505a0a0f
Leon-ha <lekileo@gmail.com>
2024-12-02 13:04:53 +0100

    feat(fe/dropdowns): add nested dropdown visualization
    Instead of showing only files and listing each as a newline, added
    nested folder visualization.
    
    Issue ref.: #4

--- JellyFork/obj/Release/net8.0/JellyFork.AssemblyInfo.cs
+++ JellyFork/obj/Release/net8.0/JellyFork.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JellyFork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffc8fa7d7870eac133172b00c38f6f977e3de79c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e077fdb4b5d30870d78ab502010b3d7c3b2f84d")]
 [assembly: System.Reflection.AssemblyProductAttribute("JellyFork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JellyFork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
